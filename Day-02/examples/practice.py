aeb0af99170c561b2446aa0a6e973395cd86f9e2
Rajat Chauhan <chum.cce@gmail.com>
2023-10-23 21:51:48 +0530

    day 2 practice

--- Day-02/examples/practice.py
+++ Day-02/examples/practice.py
@@ -6,17 +6,18 @@ a = "https github.com rajatchauhan git/python-for-devops, git"
 # b = re.split(pattern, a)
 # print(b)
 
-# pattern = r"github"
+pattern = r"git"
 
-# b = re.search(pattern, a)
-# if b:
-#     print("pattern found", b.group())
-# else:
-#     print("pattern not found")
-
-pattern = r"github.com"
-b = re.match(pattern, a)
+b = re.search(pattern, a)
 if b:
-    print("match found", b.list())
+    print("pattern found", b.group())
 else:
-    print("match not found")
+    print("pattern not found")
+
+# pattern = r"github.com"
+# b = re.match(pattern, a)
+# if b:
+#     print("match found", b.group())
+# else:
+#     print("match not found")
+
