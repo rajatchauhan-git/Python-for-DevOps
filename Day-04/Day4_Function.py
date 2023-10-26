13c4e5237f7d9b434305ac96c0e94be7aaf7608d
Rajat Chauhan <chum.cce@gmail.com>
2023-10-27 02:03:54 +0530

    practice for day 4 || functions and modules

---
+++ Day-04/Day4_Function.py
@@ -0,0 +1,61 @@
+#program for add, sub , multi, and divide without function.
+# num1 = 2
+# num2 = 10
+
+# addition = num1 + num2
+# print("Value of addition is ", str(addition))
+
+# sub = num1 - num2
+# print(sub)
+
+# multi = num1 * num2
+# print(multi)
+
+
+#same program with defining functions
+# num1 = 10
+# num2 = 5
+
+# def addition():
+#     add = num1 + num2
+#     print(add)
+
+# def substration():
+#     sub = num1 - num2
+#     print(sub)
+
+# def multiplication():
+#     multi = num1 * num2
+#     print(multi)
+
+# def division():
+#     div = num1 / num2
+#     print(div)
+
+# addition()
+# substration()
+# multiplication()
+# division()
+
+#make your program more modular by calling the values in functions
+
+def addition(num1, num2):
+    add = num1 + num2
+    return add
+
+def substration(num1, num2):
+    sub = num1 - num2
+    return sub
+
+def multiplication(num1, num2):
+    multi = num1 * num2
+    return multi
+
+def division(num1, num2):
+    div = num1 / num2
+    return div
+
+print("sum of values is: ", str(addition(5, 10)))
+print(substration(5, 10))
+print(multiplication(5, 10))
+print(division(5, 10))
---
+++ Day-04/Day4_module.py
@@ -0,0 +1,3 @@
+import Day4_Function as basic_calc
+
+basic_calc.addition(10, 20)
